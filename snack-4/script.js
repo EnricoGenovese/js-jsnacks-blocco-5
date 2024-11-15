04193675d74a9de083b54e56d7a0570c4e21fd75
EnricoGenovese <enricogeno@libero.it>
2024-11-15 14:56:25 +0100

    add snack-4 method;

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,11 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+let names = [];
+posts.forEach((post) => {
+  names.push(post.author);
+
+})
+console.log(names);
+
